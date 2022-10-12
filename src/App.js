ebc6a272cce26b5afc1a6bc68da26b991786c097
naser23 <naserabdulkadir0@gmail.com>
2022-10-12 16:50:03 -0400

    Working on navbar styling and will start mobile navbar soon.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from "react";
+import LogoDark from "../assets/shared/desktop/logo-dark.png";
+import HamburgerIcon from "../assets/shared/mobile/icon-hamburger.svg";
+import CloseIcon from "../assets/shared/mobile/icon-close.svg";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="navbar font-40">
+      {/* <div className="logoArea">
+      </div> */}
+      <img className="logo" src={LogoDark} alt="Light logo" />
+      <img className="hamburgerIcon" src={HamburgerIcon} alt="Hamburger Icon" />
+
+      <nav className="navLinks">
+        <ul className="links font-20">
+          <li className="link">OUR COMPANY</li>
+          <li className="link">LOCATIONS</li>
+          <li className="link">CONTACT</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
